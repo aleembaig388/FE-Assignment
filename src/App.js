812ff62b701d4de60ed38c45a5d227708e556a6f
aleembaig388 <aleem.baig388@hotmail.com>
2020-12-23 02:49:00 +0500

    Push Latest Code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
+
+  const [isBold, setIsBold] = useState(false);
+  const [isItalic, setIsItalic] = useState(false);
+  const [isList, setIsList] = useState(false);
 
   const handleCommand = (command, value) => {
     document.execCommand(command, false, value);
@@ -13,9 +17,9 @@ const App = () => {
       <h1>FE Assignment (Editor)</h1>
       <div className="content-wapper">
         <div className="header-panel">
-          <button title="Make Bold Highlighted Text" onClick={() => { handleCommand('bold'); }}>Bold</button>
-          <button title="Make Italic Highlighted Text" onClick={() => { handleCommand('italic'); }} >Italic</button>
-          <button title="Make Unordered List of Highlighted Text"  onClick={() => { handleCommand('insertUnorderedList'); }}>List</button>
+          <button className={ isBold ? "selected" : "" } title="Make Bold Highlighted Text" onClick={() => { handleCommand('bold'); setIsBold(!isBold); }}>Bold</button>
+          <button className={ isItalic ? "selected" : "" } title="Make Italic Highlighted Text" onClick={() => { handleCommand('italic'); setIsItalic(!isItalic); }} >Italic</button>
+          <button className={ isList ? "selected" : "" }  title="Make Unordered List of Highlighted Text"  onClick={() => { handleCommand('insertUnorderedList'); setIsList(!isList); }}>List</button>
           <p><button title="Click Here to make URL" onClick={() => { handleCommand('createlink', document.getElementById('txtURL').value); }}>URL</button> <input id="txtURL" type="url" placeholder="www.example.com" /></p>
           <input type="color" id="colorPalette" onChange={(e) => { handleCommand('foreColor', e.target.value); }} name="colorPalette"/>
           </div>
